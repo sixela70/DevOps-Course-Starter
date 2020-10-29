d6930ac15b78850925bfed012100c3fb091d31ae
sixela70 <alexis.housego@gmail.com>
2020-10-29 15:43:17 +0000

    Quick tidy up out of time on this Would like to know how to do a submit from clicking a checkbox rather than having to press a button Would like to see a professional type solution to learn from

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,13 +3,14 @@ from todo_app.data.session_items import add_item, get_items, mark_item_done, mar
 from flask import Flask, render_template, redirect
 from todo_app.flask_config import Config
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-        return render_template('index.html',items=get_items()) 
+    return render_template('index.html', items=get_items())
+
 
 @app.route("/additem", methods=['GET', 'POST'])
 def additem():
@@ -18,6 +19,7 @@ def additem():
     if isvalid(new_todo_item):
         add_item(new_todo_item)
     return redirect('/')
+
 
 @app.route("/update", methods=['GET', 'POST'])
 def update():
@@ -28,18 +30,21 @@ def update():
     updateDoneStatus()
     return redirect('/')
 
+
 def updateDoneStatus():
     items = get_items()
     for item in items:
-        print(item['id'] )
-        ischeckedkey = "to_do_chk_"+str(item['id']) 
+        print(item['id'])
+        ischeckedkey = "to_do_chk_"+str(item['id'])
         if request.form.get(ischeckedkey):
             mark_item_done(item)
         else:
             mark_item_not_done(item)
 
-def isvalid( new_todo_item):
+
+def isvalid(new_todo_item):
     return len(new_todo_item) != 0
+
 
 if __name__ == '__main__':
     app.run()
